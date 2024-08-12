9d880bacd7c281e19e8e0962df841ca208f3bd71
koubek <kouv13@vse.cz>
2024-08-12 23:53:31 +0200

    db references shortened

--- database/migrations/2024_08_12_212151_create_reviews_table.php
+++ database/migrations/2024_08_12_212151_create_reviews_table.php
@@ -22,6 +22,8 @@ return new class extends Migration {
 
             $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
 
+            $table->foreignId('book_id')
+
         });
     }
 
