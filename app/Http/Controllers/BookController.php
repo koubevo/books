520f9ca52081c34334f70c3a6bc9cfe0d822c4a3
koubek <kouv13@vse.cz>
2024-08-15 16:47:20 +0200

    caching

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -25,7 +25,10 @@ class BookController extends Controller
             default => $books->latest()
         };
 
-        $books = $books->get();
+        //$books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
